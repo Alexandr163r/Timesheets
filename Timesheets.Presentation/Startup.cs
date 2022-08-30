45f255303a9ed6e899ddec1b1ef543b004e5056f
Alexandr Ulianov <ulyanov_alexandr_job@mail.ru>
2022-08-30 06:29:19 +0400

    Add Startup.cs

---
+++ Timesheets.Presentation/Startup.cs
@@ -0,0 +1,37 @@
+namespace Timesheets.Presentation;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    private IConfiguration Configuration { get; }
+
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        
+        services.AddEndpointsApiExplorer();
+        
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseEndpoints(
+            endpoints => { endpoints.MapControllers(); });
+    }
+}
