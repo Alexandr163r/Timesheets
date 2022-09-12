a2c932e1a8b70768206af098550e85af097b1148
Alexandr Ulianov <ulyanov_alexandr_job@mail.ru>
2022-09-12 14:42:45 +0400

    Add DataTimeConverter.cs

---
+++ Timesheets.Presentation/Converter/DataTimeConverter.cs
@@ -0,0 +1,17 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Timesheets.Presentation.Converter;
+
+public class DateTimeConverter : JsonConverter<DateTime>
+{
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateTime.Parse(reader.GetString());
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString("dd/MM/yyyy H:mm"));
+    }
+}
